54ff1e1ff6005ea13e2efcf93d5ef6ceac05c6f7
Ronak Nareshbhai Parmar <ronak.parmar@intimetec.com>
2024-03-26 08:54:32 +0530

    Fixed UI for mobile view

--- src/components/briva-enterprise.vue
+++ src/components/briva-enterprise.vue
@@ -1,14 +1,20 @@
 <template>
   <v-container fluid>
-    <v-app-bar app dark color="primary">
-      <v-spacer></v-spacer>
-      <v-toolbar-items>
-        <v-btn :class="{ 'highlighted-btn': isHomePage }" text @click="navigateToPage('home')">Home</v-btn>
-        <v-btn :class="{ 'highlighted-btn': isShopPage }" text @click="navigateToPage('shop')">Shop</v-btn>
-        <v-btn :class="{ 'highlighted-btn': isAboutPage }" text @click="navigateToPage('about')">About Us</v-btn>
-        <v-btn :class="{ 'highlighted-btn': isContactPage }" text @click="navigateToPage('contact')">Contact</v-btn>
-      </v-toolbar-items>
-    </v-app-bar>
+    <v-row>
+      <v-toolbar dark color="primary">
+        <v-spacer></v-spacer>
+        <v-toolbar-items>
+          <v-btn :class="{ 'highlighted-btn': isHomePage }" text @click="navigateToPage('home')"
+            min-width="64">Home</v-btn>
+          <v-btn :class="{ 'highlighted-btn': isShopPage }" text @click="navigateToPage('shop')"
+            min-width="64">Shop</v-btn>
+          <v-btn :class="{ 'highlighted-btn': isAboutPage }" text @click="navigateToPage('about')" min-width="64">About
+            Us</v-btn>
+          <v-btn :class="{ 'highlighted-btn': isContactPage }" text @click="navigateToPage('contact')"
+            min-width="64">Contact</v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+    </v-row>
 
     <v-row v-if="isHomePage">
       <v-col cols="12" class="text-center">
@@ -18,7 +24,7 @@
         </p>
       </v-col>
       <v-col cols="12" class="d-flex justify-center">
-        <v-img src="briva-logo.io.png" contain height="450"></v-img>
+        <v-img src="briva-logo.io.png" contain height="350"></v-img>
       </v-col>
     </v-row>
 
